a3541b8033b7a102acda243dd4edea27ebe2655a
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2024-03-10 20:01:11 +0200

    add  encoder

---
+++ model/encoder.py
@@ -0,0 +1,49 @@
+import torch.nn as nn
+import math
+
+import logging
+
+
+class EnConvBlock(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, kernel: int = 3, stride: int = 2):
+        super(EnConvBlock, self).__init__()
+
+        self.conv_block = nn.Sequential(
+            nn.Conv3d(in_channels, out_channels, kernel, padding=kernel//2),
+            nn.ELU(),
+            nn.Conv3d(out_channels, out_channels, kernel, stride=stride, padding=kernel//2),
+            nn.ELU()
+        )
+    
+    def forward(self, input):
+        return self.conv_block(input)
+
+
+class Encoder(nn.Module):
+    def __init__(self, channels: int, blocks: int, in_size: tuple[int, int, int], z_dim: int):
+        super(Encoder, self).__init__()
+
+        self.input_conv = nn.Sequential(
+            nn.Conv3d(1, channels, 3, padding=1),
+            nn.ELU()
+        )
+
+        conv_layers = []
+        for i in range(blocks):
+            in_channels = channels * (2**i)
+            out_channels = in_channels*2
+            conv_layers.append(EnConvBlock(in_channels, out_channels))
+            logging.info(f"Added convolution block to encoder (channels: {in_channels}->{out_channels})")
+        self.encode = nn.Sequential(*conv_layers)
+
+        flat_h, flat_w, flat_d = [math.ceil(size/(2**blocks)) for size in in_size]
+        self.flat_size = flat_h*flat_w*flat_d*channels*(2**blocks)
+        self.dense = nn.Linear(self.flat_size, z_dim)
+
+
+    def forward(self, x):
+        x = self.input_conv(x)
+        encoded = self.encode(x)
+        flatten = encoded.view(-1, self.flat_size)
+        z = self.dense(flatten)
+        return z
