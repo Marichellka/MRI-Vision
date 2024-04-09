89dadde8456c2c3063e8649d465e3692668ddd16
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2024-04-09 19:06:18 +0300

    add empty wpf project

---
+++ MRI-Vision/MRI-Vision.UI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MRI_Vision.UI;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
