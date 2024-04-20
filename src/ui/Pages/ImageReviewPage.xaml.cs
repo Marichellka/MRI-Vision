1e3506e187652682f6786a29dcecd3fec079914c
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2024-04-20 18:14:16 +0300

    add image review page

---
+++ src/ui/Pages/ImageReviewPage.xaml.cs
@@ -0,0 +1,109 @@
+﻿using MRI_Vision.UI.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+using WpfAnimatedGif;
+using System.IO;
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+
+namespace MRI_Vision.UI.Pages
+{
+    /// <summary>
+    /// Interaction logic for ImageReviewPage.xaml
+    /// </summary>
+    public partial class ImageReviewPage : Page
+    {
+        private Dictionary<MRIPictureOrientation, MRIPicture> _pictures;
+        private MRIPictureOrientation _currentOrientation;
+
+        public ImageReviewPage(string filePath)
+        {
+            InitializeComponent();
+            LoadImageAsync(filePath);
+        }
+
+        private async void LoadImageAsync(string filePath)
+        {
+            var MRIPicture = await Task.Run(() => new MRIPicture(filePath));
+            _pictures = new();
+            _pictures.Add(MRIPicture.Orientation, MRIPicture);
+            _currentOrientation = MRIPicture.Orientation;
+            SetScrollBar(0);
+            PictureOrientationComboBox.SelectedItem = _currentOrientation;
+        }
+
+        private void SetSliceImage(Bitmap slice)
+        {
+            MemoryStream memory = new MemoryStream();
+            slice.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+            memory.Position = 0;
+            BitmapImage imageSource = new BitmapImage();
+            imageSource.BeginInit();
+            imageSource.StreamSource = memory;
+            imageSource.CacheOption = BitmapCacheOption.OnLoad;
+            imageSource.EndInit();
+            ImageBehavior.SetAnimatedSource(UploadedImage, imageSource);
+        }
+
+        private void SetScrollBar(int sliceInd) // TODO: rename
+        {
+            ImageScrollBar.Maximum = _pictures[_currentOrientation].Length - 1;
+            ImageScrollBar.Value = sliceInd;
+            SetSliceImage(_pictures[_currentOrientation][sliceInd]);
+        }
+
+        private void OnScroll(object sender, RoutedEventArgs e)
+        {
+            if(_pictures is null) return;
+
+            int sliceIndex = (int)ImageScrollBar.Value;
+            SetSliceImage(_pictures[_currentOrientation][sliceIndex]);
+        }
+
+        private void PictureOrientationComboBoxSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (_pictures is null) return;
+
+            MRIPictureOrientation orientation = (MRIPictureOrientation) PictureOrientationComboBox.SelectedItem;
+            if (orientation == _currentOrientation)
+                return;
+            
+            if(_pictures.ContainsKey(orientation))
+            {
+                _currentOrientation = orientation;
+            }
+            else
+            {
+                var newImage = _pictures[_currentOrientation].RotatePicture(orientation);
+                _pictures.Add(orientation, newImage);
+                _currentOrientation = orientation;
+            }
+
+            int newInd = Math.Clamp((int)ImageScrollBar.Value, 0, _pictures[_currentOrientation].Length);
+            SetScrollBar(newInd);
+        }
+    }
+
+    class PictureOrientation: ObservableCollection<MRIPictureOrientation>
+    {
+        public PictureOrientation()
+        {
+            Add(MRIPictureOrientation.Front);
+            Add(MRIPictureOrientation.Side);
+            Add(MRIPictureOrientation.Top);
+        }
+    }
+}
---
+++ src/ui/Utils/MRIPicture.cs
@@ -0,0 +1,131 @@
+﻿using Python.Runtime;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+
+namespace MRI_Vision.UI.Utils
+{
+    public enum MRIPictureOrientation
+    {
+        Front,
+        Side,
+        Top
+    }
+
+    internal class MRIPicture
+    {
+        public int Length => _imageData.Length;
+        public MRIPictureOrientation Orientation => _orientation;
+
+        private float[][][] _imageData;
+        private Bitmap[] _bitmapSlices;
+        private float _max;
+        private MRIPictureOrientation _orientation;
+        private int[] _size;
+        private const string ModelModulePath = @"C:\Users\maric\Studying\Diploma\Project\MRI-Vision\src\network\model\utils\loader.py";
+
+        public MRIPicture(string path)
+        {
+            _imageData = ReadImage(path);
+            _max = _imageData.Max(x => x.Max(y => y.Max()));
+            _size = new[] { _imageData.Length, _imageData[0].Length, _imageData[0][0].Length };
+            _orientation = MRIPictureOrientation.Side;
+            _bitmapSlices = GetBitmapSlices();
+        }
+
+        public MRIPicture(float[][][] data, int[] size, float max, MRIPictureOrientation orientation)
+        {
+            _imageData = data;
+            _max = max;
+            _size = size;
+            _orientation = orientation;
+            _bitmapSlices = GetBitmapSlices();
+        }
+
+        private float[][][] ReadImage(string path)
+        { 
+            using var _ = Py.GIL();
+
+            dynamic os = Py.Import("os");
+            dynamic sys = Py.Import("sys");
+            sys.path.append(os.path.dirname(ModelModulePath));
+
+            dynamic loader = Py.Import(Path.GetFileNameWithoutExtension(ModelModulePath));
+
+            return (float[][][])loader.load_image(path);
+        }
+
+        private Bitmap[] GetBitmapSlices()
+        {
+            Bitmap[] slices = new Bitmap[_imageData.Length];
+            for (int i = 0; i < slices.Length; i++)
+            {
+                slices[i] = GetBitmapSlice(i);
+            }
+            return slices;
+        }
+
+
+        private Bitmap GetBitmapSlice(int index)
+        {
+            float[][] slice = _imageData[index];
+            Bitmap bitmap = new Bitmap(_size[1], _size[2]);
+            for (int i = 0; i < _size[1]; i++)
+            {
+                for (int j = 0; j < _size[2]; j++)
+                {
+                    int color = (int)(slice[i][j] * 255 / _max);
+                    Color pixelColor = Color.FromArgb(color, color, color);
+                    bitmap.SetPixel(i, j, pixelColor);
+                }
+            }
+
+            return bitmap;
+        }
+
+        public MRIPicture RotatePicture(MRIPictureOrientation newOrientation)
+        {
+            if (_orientation == newOrientation) return this;
+
+            int rotation = _orientation - newOrientation;
+            rotation = rotation < 0 ? 3 + rotation : rotation;
+
+            int[] newSize = RotateArray(_size, rotation);
+            float[][][] newData = new float[newSize[0]][][];
+            for (int i = 0; i < newSize[0]; i++)
+            {
+                newData[i] = new float[newSize[1]][];
+                for (int j = 0; j < newSize[1]; j++)
+                {
+                    newData[i][j] = new float[newSize[2]];
+                    for (int k = 0; k < newSize[2]; k++)
+                    {
+                        int[] indexes = RotateArray(new[] {i, j, k}, _size.Length-rotation);
+                        newData[i][j][k] = _imageData[indexes[0]][indexes[1]][indexes[2]];
+                    }
+                }
+            }
+
+            return new MRIPicture(newData, newSize, _max, newOrientation);
+        }
+
+        private int[] RotateArray(int[] array, int rotation)
+        {
+            int[] newArray = new int[array.Length];
+            for (int i = 0; i < array.Length - rotation; i++)
+            {
+                newArray[i] = array[i + rotation];
+            }
+            for (int i = array.Length - rotation; i < array.Length; i++)
+            {
+                newArray[i] = array[i + rotation - array.Length];
+            }
+            return newArray;
+        }
+
+        public Bitmap this[int index]
+        {
+            get => _bitmapSlices[index];
+        }
+    }
+}
