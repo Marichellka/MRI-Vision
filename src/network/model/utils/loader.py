bc09d547c2e07eac44fe59dfd08b5b9c06187d97
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2024-04-21 21:08:32 +0300

    add module to use in ui

---
+++ src/network/model/utils/loader.py
@@ -0,0 +1,68 @@
+import torch
+from monai.data import CacheDataset, DataLoader
+from monai.transforms import (
+    Compose,
+    LoadImage,
+    AddChannel,
+    ScaleIntensity,
+    EnsureType,
+    Resize
+)
+
+import sys
+sys.path.append('C:\\Users\\maric\\Studying\\Diploma\\Project\\MRI-Vision\\src\\network\\')
+from model.autoencoder import AutoEncoder
+
+size = (160, 160, 96)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def load_model(path: str) -> AutoEncoder:
+    model = AutoEncoder(size, device, blocks=5)
+    model.load(path)
+
+    return model
+
+
+def load_data(files: list[str]) -> CacheDataset:
+    pre_process = Compose([
+        LoadImage(image_only=True),
+        AddChannel(),
+        Resize(spatial_size=size), 
+        ScaleIntensity(),
+        EnsureType(),
+    ])
+
+    dataset = CacheDataset(files, pre_process)
+    return dataset  
+
+
+def load_image(path: str, size: tuple[int, int, int] = (160, 160, 96)):
+    loadImage = Compose([
+        LoadImage(image_only=True), 
+        AddChannel(),
+        Resize(spatial_size=size),
+    ])
+    [image] = loadImage(path).array
+    return image.tolist()
+
+
+def process_image(path: str, model: AutoEncoder) -> tuple:
+    loadImage = Compose([
+        LoadImage(image_only=True),
+        AddChannel(),
+        Resize(spatial_size=(160, 160, 96)), 
+        ScaleIntensity(),
+        EnsureType(),
+        AddChannel()
+    ])
+    image = loadImage(path)
+    with torch.no_grad():
+        input = image.to(model.device)
+        encoded = model.encoder(input)
+        restored = model.decoder(encoded)
+
+        anomaly = abs(input-restored)
+    
+    [[input]] = input
+    [[anomaly]] = anomaly
+    return input.tolist(), anomaly.tolist()
