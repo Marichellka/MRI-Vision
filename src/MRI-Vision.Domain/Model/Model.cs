a511d79665c946c88f01912ea7777731b35cb5f7
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2024-05-26 14:38:39 +0300

    chnage model path

--- src/MRI-Vision.Domain/Model/Model.cs
+++ src/MRI-Vision.Domain/Model/Model.cs
@@ -28,6 +28,7 @@ public class Model
         {
             dynamic os = Py.Import("os");
             dynamic sys = Py.Import("sys");
+            sys.path.append(os.path.dirname(_modelModulePath));
 
             dynamic loader = Py.Import(Path.GetFileNameWithoutExtension(_modelModulePath));
 
